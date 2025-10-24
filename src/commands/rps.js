84275c715a54a2dd9a820cac81187cc2a0a1f53d
WorthyOtter <67753532+ShadowNinja97@users.noreply.github.com>
2025-10-24 00:07:19 +0000

    A bunch of deletions :)

--- src/commands/rps.js
+++ src/commands/rps.js
@@ -1,11 +1,6 @@
 // src/commands/rps.js
 import { InteractionResponseType } from "discord-interactions";
-import {
-  RPS_CHOICES,
-  botPick,
-  judge,
-  formatResult,
-} from "../games/rps/rules.js";
+import { RPS_CHOICES, botPick, judge } from "../games/rps/rules.js";
 import { validateAndLockBet } from "../economy/bets.js";
 import { getBalance } from "../economy/db.js";
 import { t } from "../core/i18n.js";
@@ -14,8 +9,13 @@ export async function execute(interaction) {
   const ctx = interaction.context; // 0=guild, 1=bot DM, 2=user DM
   const userId = ctx === 0 ? interaction.member.user.id : interaction.user.id;
 
-  const player = interaction.data.options?.find((o) => o.name === "choice")?.value;
-  const bet = Number(interaction.data.options?.find((o) => o.name === "bet")?.value ?? 0) | 0;
+  const player = interaction.data.options?.find(
+    (o) => o.name === "choice"
+  )?.value;
+  const bet =
+    Number(
+      interaction.data.options?.find((o) => o.name === "bet")?.value ?? 0
+    ) | 0;
 
   if (!RPS_CHOICES.includes(player)) {
     return {
--- src/events/ready.js
+++
@@ -1,5 +0,0 @@
-export const name = "ready";
-export const once = true;
-export function execute(client) {
-  console.log(`🤖 Logged in as ${client.user.tag}`);
-}
